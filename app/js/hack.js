b1094a31d6d95b3a178a1d3e8ae3ee9c3f5cbca9
samphree <sam.acree@gmail.com>
2014-11-09 15:04:25 -0600

    css and js splashy stuff

--- app/js/hack.js
+++ app/js/hack.js
@@ -2,4 +2,8 @@ $( document ).ready(function() {
 	$('.side-icon').click(function(){
 		$('.sidebar-sub').toggleClass("open");
 	});
+	$('.splash-btn').click(function(){
+		$('#splash').hide();
+		$('#app').show();
+	});
 });
