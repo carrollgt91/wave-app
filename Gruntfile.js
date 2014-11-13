1c6fd380b593acd8807c2bf1102ceed6bc320cfd
Grayson Carroll <carrollgt91@gmail.com>
2014-11-12 22:01:09 -0600

    Adds grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    aws: grunt.file.readJSON('creds.json'),
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.AWSAccessKeyId %>', // Use the variables
+        secretAccessKey: '<%= aws.AWSSecretKey %>', // You can also use env variables
+        region: 'us-east-1',
+        uploadConcurrency: 5, // 5 simultaneous uploads
+        downloadConcurrency: 5 // 5 simultaneous downloads
+      },
+      production: {
+        options: {
+          bucket: 'www.waveplayer.co'
+        },
+        files: [
+          { expand: true, cwd: 'app/', src: ['**'], action: 'upload', dest: '.', differential: true }
+        ]
+      } 
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-aws-s3');
+
+  // Default task(s).
+  grunt.registerTask('deploy', ['aws_s3']);
+  
+
+};
