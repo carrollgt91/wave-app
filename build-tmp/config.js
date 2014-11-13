2135bcdc1b6ec45a13e20ac9d5017e24a22eae52
Grayson Carroll <carrollgt91@gmail.com>
2014-11-13 15:28:22 -0600

    Patches build config

--- build-tmp/config.js
+++ build-tmp/config.js
@@ -1,5 +1,5 @@
 var config = {
   rootUrl: "http://malachite-api.herokuapp.com/api",
-  clientId: '251c9152fb3757d609504877ed494ae0',
+  clientId: "58bb956d274f4b5af9637dbbfe47297f",
   redirectUri: "http://waveplayer.co/callback.html"
 };
